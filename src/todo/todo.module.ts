ad41fa4cc1c1e873e94d9e67ce822f8f94571039
kouyama0201 <kami19900201@gmail.com>
2024-08-27 10:28:28 +0900

    8. Prisma Service

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaService],
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [TodoService],
 })
 export class TodoModule {}
